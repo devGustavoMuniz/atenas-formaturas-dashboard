f9480ac1e0c57d9c62f894ec85e73277a371c5b7
Gustavo Muniz <gustavomuniz545@gmail.com>
2025-07-07 19:33:58 -0300

    feat: adicionado botao de retornar a home na tela de login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next"
 import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 import { LoginForm } from "@/components/auth/login-form"
 
 export const metadata: Metadata = {
@@ -10,6 +13,16 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="https://atenasformaturas.com.br"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 flex items-center md:right-8 md:top-8"
+        )}
+      >
+        <ArrowLeft className="mr-2 h-4 w-4" />
+        Voltar para a home
+      </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
